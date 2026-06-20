77b8d522b04b098fabc9e7b408c184847ab516c2
Bogdan L'gotin <llwizard@keemail.me>
2026-06-20 21:04:17 +0500

    Add -j param to speed up project builds.

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
 arg=${1:-} # poker_client OR poker_server
 
 if [[ -n $arg ]]; then
-  cmake --build . --target $arg
+  cmake --build . --target $arg -j$(nproc)
 else
-  cmake --build .
+  cmake --build . -j$(nproc)
 fi
